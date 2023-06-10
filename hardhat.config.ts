1413639f60891403820d83337aa38b14851ff564
Robert Brada <bert.blader@gmail.com>
2023-06-10 15:03:17 +0200

    feat: add scroll alpha config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    scrollAlpha: {
+      url: "https://alpha-rpc.scroll.io/12" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
